a721b0f3e9d25514395e506c86a3784a1d9f639a
Marco Brandizi <brandizi@ebi.ac.uk>
2014-09-18 16:41:46 +0100

    Adjusting dependencies needed by BioSD. Fixing the deployment scripts.

--- web-ui/deploy_scripts/war2stage.sh
+++ web-ui/deploy_scripts/war2stage.sh
@@ -1,3 +1,4 @@
-scp brandizi@ebi-001:/tmp/biosamples.war /nfs/public/rw/webadmin/tomcat/bases/rdf/tc-rdf-sparql_dev/deploy/biosamples.war
-
-
+# Must be run as rdf_adm from ebi-001, after the copy of biosamples.war onto ebi-001 (see deploy_lodestar.sh)
+# Deploys the war on the stage server ves-hx-f2
+ssh -Yf brandizi@ebi-001 'chmod ugo+r /tmp/biosamples.war'
+scp /tmp/biosamples.war ves-hx-f2:/nfs/public/rw/webadmin/tomcat/bases/rdf/tc-rdf-sparql_dev/deploy/biosamples.war
